863421aecc07f003a258f12a33cd3413139b9610
kaiWhitney_ <kaisankyachan@gmail.com>
2022-02-20 23:29:30 -0700

    first api code

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,18 @@
-var cityNameInput = document.querySelector("#city");
+let searchedCity = document.getElementById("city")
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
 
-
-$('#submit').click(function(event) {
-    event.preventDefault();
-    if (!cityNameInput.value) {
-      $("#missing-param-modal").modal("show");
-    } else if (cityNameInput.value) {
-      var citySearch = cityNameInput.value
-      .trim()
-      .toLowerCase()
-      .replaceAll(" ", "+");
-      window.location.href = "./HTML/events.html?city=" + citySearch;
-    }
+function apiCall(){
+  var myCity = document.getElementById('city').value
+  console.log(myCity)
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${myCity}&APPID=406e0928d4f7e01f9fc7227e0801a8e4`)
+  .then(response=> {
+    return response.json();
+  }).then(data =>{
+    return document.getElementById('searched-city').innerHTML = myCity, 
+    document.getElementById('todays-date').innerHTML = date, 
+    document.getElementById('todays-temp').innerHTML = data.main.temp, 
+    document.getElementById('todays-wind').innerHTML = data.wind.speed, 
+    document.getElementById('todays-humidity').innerHTML = data.main.humidity;
+  })
+}
